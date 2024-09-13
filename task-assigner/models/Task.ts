594eb06ed1756589e42614ca33bf67ea00b68e30
Harshit Panwar <harshitpanwar234567@gmail.com>
2024-09-13 06:28:53 +0530

    add project and view project for project managers

--- task-assigner/models/Task.ts
+++ task-assigner/models/Task.ts
@@ -1,14 +1,16 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose from 'mongoose';
 
-const taskSchema = new Schema({
+const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, enum: ['text', 'audio', 'video', 'image'], required: true },
+  question: { type: Object, required: true }, // Store question and options
   media_uri: { type: [String], required: false },
-  project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
-  annotater: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  answer_type: { type: String, enum: ['radio_button', 'text_field', 'checkbox_single', 'checkbox_multiple'], required: true },
-  answer: { type: [String], required: false },
-  status: { type: String, enum: ['pending', 'accepted', 'rejected', 'reassigned'], default: 'pending' }
+  project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
+  annotator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  answer_type: { type: String, enum: ['text_field', 'checkbox_single', 'checkbox_multiple'], required: true },
+  answer: { type: Object, required: false },
+  status: { type: String, enum: ['pending', 'accepted', 'rejected', 'reassigned'], default: 'pending' },
 });
 
-export const Task = models.Task || model('Task', taskSchema);
+const Task = mongoose.models.Task || mongoose.model('Task', taskSchema);
+export default Task;
